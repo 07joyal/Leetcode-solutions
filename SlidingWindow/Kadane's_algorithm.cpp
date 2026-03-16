3aa7b34d143a18857695e17144fda1db31d0c3a9
Joyalpreet Kaur <joyyy0016@gmail.com>
2026-03-16 23:07:23 +0530

    Solve Leetcode 53 - Maximum Subarray using Kadane's Algorithm
    Implemented an optimal O(n) solution for the Maximum Subarray problem.
    Time Complexity: O(n)
    Space Complexity: O(1)
    Approach:
    -  i maintained a running sum while iterating through the array.
    - Updated the maximum subarray sum whenever the running sum  was greater than the maximum sum
    - Reset the running sum to 0 whenever it becomes negative, because a negative prefix cannot contribute to a larger sum.

--- SlidingWindow/Kadane's_algorithm.cpp
+++ SlidingWindow/Kadane's_algorithm.cpp
@@ -3,4 +3,33 @@
 //Time complexity-O(n)
 //Space complexity -O(1)
 //Approach- kadane's algorithm
-//I derived this approach myself
+// Note: I derived the  core idea (tracking running sum and resetting when negative)
+// myself during problem solving ; later i learned it corresponds to Kadane's Algorithm.
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int left=0; //variable to iterate through array
+        int sum=0; //current sum
+        int max_sum=INT_MIN; //initialize max sum to minimum value
+        
+        while(left<n){
+             sum=sum+nums[left];
+           
+             if(sum>max_sum){
+                   max_sum=sum; //updating max sum
+                            }
+            
+            if(sum<0){
+                        sum=0; //if sum is negative,drop the sum to 0
+                      
+                    }
+                    
+                        left++; //move to next element
+                   
+        }
+        
+        return max_sum;
+    }
+};
