090c776fd4e612fd4dbb7e50ebf000ee0d793e23
Joyalpreet Kaur <joyyy0016@gmail.com>
2026-08-02 00:42:36 +0530

    Add node counting functionality for binary trees
    Implement a solution to count the nodes in a binary tree using left and right depth calculations.

---
+++ trees/count_tree_nodes.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+int left1(TreeNode* root){
+    int count=0;
+    while(root){
+        root=root->left;
+        count++;
+    }
+    return count;
+}
+int right1(TreeNode* root){
+    int count=0;
+    while(root){
+        root=root->right;
+        count++;
+    }
+    return count;
+}
+int count1(TreeNode* root){
+    if(root==NULL){
+        return 0 ;
+       }
+   int left= left1(root);
+   int right= right1(root);
+    if(left==right){
+        return pow(2, left)-1;
+    }
+       return 1+ count1(root->left) + count1(root->right);
+}
+    int countNodes(TreeNode* root) {
+      if(root==NULL){
+        return 0;
+       }
+      return count1(root);
+        
+    }
+};
