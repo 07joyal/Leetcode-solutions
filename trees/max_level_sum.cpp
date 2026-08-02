b5e4a82bf1ade8f77dcf1a7293a48a5db81c7475
Joyalpreet Kaur <joyyy0016@gmail.com>
2026-08-02 11:45:17 +0530

    Implement max level sum function for binary tree

---
+++ trees/max_level_sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        int sum=0;
+        vector<int>arr;
+        queue<TreeNode* >q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                TreeNode* a=q.front();
+                sum+=a->val;
+                q.pop();
+                
+            if(a->left!=NULL){
+                q.push(a->left);
+            }
+            if(a->right!=NULL){
+                q.push(a->right);
+            }
+                 }
+        arr.push_back(sum);
+        sum=0;
+        }
+        int max=INT_MIN;
+        int idx;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]>max){
+                max=arr[i];
+                idx=i;
+            }
+        }
+   return idx+1;;
+    }
+};
