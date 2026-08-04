6074b002b2d04eca7a70e5027212a63fcd91fb54
Joyalpreet Kaur <joyyy0016@gmail.com>
2026-08-04 22:10:26 +0530

    Implement iterative preorder traversal for binary tree

---
+++ trees/preorder_iterative.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root==NULL){
+            return vector<int>();
+        }
+        stack<TreeNode*>s;
+        s.push(root);
+        vector<int>ans;
+        TreeNode* a;
+        while(!s.empty()){
+             a=s.top();
+             s.pop();
+             ans.push_back(a->val);
+             if(a->right!=NULL){
+                s.push(a->right);
+             }
+             if(a->left!=NULL){
+                s.push(a->left);
+             }
+
+        }
+        return ans;
+        
+    }
+};
